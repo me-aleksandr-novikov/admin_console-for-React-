9f38496e4c34a875179310487ff138cd5eb53889
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2023-01-11 00:03:39 +1000

    Save page

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,6 +1,10 @@
 import "../../helpers/iframeLoader.js";
 import axios from 'axios';
 import React, { Component } from 'react';
+import DOMHelper from '../../helpers/dom-helper';
+import EditorText from '../editor-text';
+import UIKit from 'uikit';
+import Spinner from '../spinner';
 
 export default class Editor extends Component {
     constructor() {
@@ -8,9 +12,12 @@ export default class Editor extends Component {
         this.currentPage = "index.html";
         this.state = {
             pageList: [],
-            newPageName: ""
+            newPageName: "",
+            loading: true
         }
         this.createNewPage = this.createNewPage.bind(this);
+        this.isLoading = this.isLoading.bind(this);
+        this.isLoaded = this.isLoaded.bind(this);
     }
 
     componentDidMount() {
@@ -19,91 +26,63 @@ export default class Editor extends Component {
 
     init(page) {
         this.iframe = document.querySelector('iframe');
-        this.open(page);
+        this.open(page, this.isLoaded);
         this.loadPageList();
     }
 
-    open(page) {
+    open(page, cb) {
         this.currentPage = page;
 
         axios
             .get(`../${page}?rnd=${Math.random()}`)
-            .then(res => this.parseStrToDOM(res.data))
-            .then(this.wrapTextNodes)
+            .then(res => DOMHelper.parseStrToDOM(res.data))
+            .then(DOMHelper.wrapTextNodes)
             .then(dom => {
                 this.virtualDom = dom;
                 return dom;
             })
-            .then(this.serializeDOMToString)
+            .then(DOMHelper.serializeDOMToString)
             .then(html => axios.post("./api/saveTempPage.php", { html }))
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing())
+            .then(() => this.injectStyles())
+            .then(cb);
     }
 
-    save() {
+    save(onSuccess, onError) {
+        this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
-        this.unwrapTextNodes(newDom);
-        const html = this.serializeDOMToString(newDom);
+        DOMHelper.unwrapTextNodes(newDom);
+        const html = DOMHelper.serializeDOMToString(newDom);
         axios
             .post("./api/savePage.php", { pageName: this.currentPage, html })
+            .then(onSuccess)
+            .catch(onError)
+            .finally(this.isLoaded);
     }
 
     enableEditing() {
         this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
-            element.contentEditable = "true";
-            element.addEventListener("input", () => {
-                this.onTextEdit(element);
-            })
+            const id = element.getAttribute("nodeid");
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+
+            new EditorText(element, virtualElement);
         });
     }
 
-    onTextEdit(element) {
-        const id = element.getAttribute("nodeid");
-        this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
-        console.log(this.virtualDom);
-    }
-
-    parseStrToDOM(str) {
-        const parser = new DOMParser();
-        return parser.parseFromString(str, "text/html");
-    }
-
-    wrapTextNodes(dom) {
-        const body = dom.body;
-        let textNodes = [];
-
-        function recursy(element) {
-            element.childNodes.forEach(node => {
-
-                if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
-                    textNodes.push(node);
-                } else {
-                    recursy(node);
-                }
-            })
-        };
-
-        recursy(body);
-
-        textNodes.forEach((node, i) => {
-            const wrapper = dom.createElement('text-editor');
-            node.parentNode.replaceChild(wrapper, node);
-            wrapper.appendChild(node);
-            wrapper.setAttribute("nodeid", i);
-        });
-
-        return dom;
-    }
-
-    serializeDOMToString(dom) {
-        const serializer = new XMLSerializer();
-        return serializer.serializeToString(dom);
-    }
-
-    unwrapTextNodes(dom) {
-        dom.body.querySelectorAll("text-editor").forEach(element => {
-            element.parentNode.replaceChild(element.firstChild, element);
-        });
+    injectStyles() {
+        const style = this.iframe.contentDocument.createElement("style");
+        style.innerHTML = `
+            text-editor:hover {
+                outline: 3px solid orange;
+                outline-offset: 8px;
+            }
+            text-editor:focus {
+                outline: 3px solid red;
+                outline-offset: 8px;
+            }
+        `;
+        this.iframe.contentDocument.head.appendChild(style);
     }
 
     loadPageList() {
@@ -126,27 +105,54 @@ export default class Editor extends Component {
             .catch(() => alert("Страницы не существует!"));
     }
 
+    isLoading() {
+        this.setState({
+            loading: true
+        })
+    }
+
+    isLoaded() {
+        this.setState({
+            loading: false
+        })
+    }
+
     render() {
-        // const {pageList} = this.state;
-        // const pages = pageList.map((page, i) => {
-        //     return (
-        //         <h1 key={i}>{page}
-        //             <a 
-        //             href="#"
-        //             onClick={() => this.deletePage(page)}>(x)</a>
-        //         </h1>
-        //     )
-        // });
+        const { loading } = this.state;
+        const modal = true;
+        let spinner;
+
+        loading ? spinner = <Spinner active /> : spinner = <Spinner />
 
         return (
-            <iframe src={this.currentPage} frameBorder="0"></iframe>
-            // <>
-            //     <input
-            //         onChange={(e) => {this.setState({newPageName: e.target.value})}} 
-            //         type="text"/>
-            //     <button onClick={this.createNewPage}>Создать страницу</button>
-            //     {pages}
-            // </>
+            <>
+                <iframe src={this.currentPage} frameBorder="0"></iframe>
+
+                {spinner}
+
+                <div className="panel">
+                    <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
+                </div>
+
+                <div id="modal-save" uk-modal={modal.toString()}>
+                    <div className="uk-modal-dialog uk-modal-body">
+                        <h2 className="uk-modal-title">Сохранение</h2>
+                        <p>Вы действительно хотите сохранить изменения?</p>
+                        <p className="uk-text-right">
+                            <button className="uk-button uk-button-default uk-modal-close" type="button">Отменить</button>
+                            <button
+                                className="uk-button uk-button-primary uk-modal-close"
+                                type="button"
+                                onClick={() => this.save(() => {
+                                    UIkit.notification({ message: 'Успешно сохранено', status: 'success' })
+                                },
+                                    () => {
+                                        UIkit.notification({ message: 'Ошибка сохранения', status: 'danger' })
+                                    })}>Опубликовать</button>
+                        </p>
+                    </div>
+                </div>
+            </>
         )
     }
 }
